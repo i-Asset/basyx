fc39cc577c6b9723433f78b57f170a236f4ee3a6
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-08-02 11:33:16 +0200

    Another test
    Change-Id: I0e57eb1d098a9c8966966d92609695e467d28106
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- ci/build_java.sh
+++ ci/build_java.sh
@@ -9,13 +9,13 @@ JAVA_SDK_CHANGED=$(echo $GIT_DIFF | grep ".*/sdks/java/.*" | wc -l)
 JAVA_COMPONENTS_CHANGED=$(echo $GIT_DIFF | grep "components/.*" | wc -l)
 JAVA_EXAMPLES_CHANGED=$(echo $GIT_DIFF | grep "examples/.*" | wc -l)
 
-if [ $((JAVA_SDK_CHANGED > 0)) ];
-then
+#if [ $((JAVA_SDK_CHANGED > 0)) ];
+#then
     $MVN -f ./sdks/java/basys.sdk/pom.xml verify
-elif [ $((JAVA_COMPONENTS_CHANGED > 0)) ];
-then
+#elif [ $((JAVA_COMPONENTS_CHANGED > 0)) ];
+#then
     $MVN -f ./components/basys.components/pom.xml verify
-elif [ $((JAVA_EXAMPLES_CHANGED > 0)) ];
-then
+#elif [ $((JAVA_EXAMPLES_CHANGED > 0)) ];
+#then
     $MVN -f ./examples/basys.examples/pom.xml verify
-fi
+#fi
