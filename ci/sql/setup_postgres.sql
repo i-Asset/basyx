8ba6b81a719c475e96633d909747ced1e527132b
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-08-21 15:21:09 +0200

    Fixed postgres init script in CI
    Change-Id: I17071c65e75f5a1651938a74ebd34ca0dbf11d25
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- ci/sql/setup_postgres.sql
+++ ci/sql/setup_postgres.sql
@@ -10,14 +10,14 @@ CREATE DATABASE "basyx-directory";
 
 CREATE SCHEMA directory;
 
-CREATE TABLE directory.directory ( "ElementRef" varchar(999999), "ElementID" varchar(999999) primary key );
+CREATE TABLE directory.directory ( "ElementRef" varchar, "ElementID" varchar primary key );
 
-INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-19', '{content.aas1}' );
+INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-19', 'content.aas1' );
 
-INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-18', '{content.aas2}' );
+INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-18', 'content.aas2' );
 
-INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-17', '{content.aas3}' );
+INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-17', 'content.aas3' );
 
-INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-16', '{content.aas4}' );
+INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-16', 'content.aas4' );
 
-INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-15', '{content.aas5}' ); 
+INSERT INTO directory.directory ( "ElementID", "ElementRef" ) VALUES ( 'urn:de.FHG:es.iese:aas:0.98:5:lab/microscope#A-15', 'content.aas5' ); 
