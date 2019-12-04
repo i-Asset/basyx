3070d6c52e39ba38789b1f6c418af192c5306570
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-12-04 21:04:52 +0100

    Assign more memory to basyx.cpp.ci container
    Change-Id: I69d7f35b42c38ca2b6d135d031039dcc5a662b29
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -8,12 +8,16 @@ GIT_DIFF=$(/usr/bin/git diff --name-only origin/CI_Test)
 
 CPP_SDK_CHANGED=$(echo $GIT_DIFF | grep ".*/basys.sdk.cc/.*" | wc -l)
 
+df -h
+echo ""
+free -m
+
 if  ((CPP_SDK_CHANGED > 0));
 then
     echo "Building and testing BaSyx C++ SDK"
 
     mkdir build && cd build
     cmake ../sdks/c++/basys.sdk.cc -DBASYX_UTILITY_PROJECTS=OFF -DBUILD_SHARED_LIBS=ON
-    make -j4 all
+    make all
     ctest
 fi
