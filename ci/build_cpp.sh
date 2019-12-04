c083e2764945a815fe4e6107b7073be3009bcb47
Thomas Psota <thomas.psota@iese.fraunhofer.de>
2019-12-04 20:13:37 +0100

    Initial commit of C++ CI configurations
    Change-Id: I2d102a0c4f10f584601ed05306effac295e942df
    Signed-off-by: Thomas Psota <thomas.psota@iese.fraunhofer.de>

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
-
-##
+#!/bin/bash
+set -e
 
 CWD=$(pwd)
 echo "CWD: $CWD"
 
 GIT_DIFF=$(/usr/bin/git diff --name-only origin/CI_Test)
 
-CPP_SDK_CHANGED=$(echo $GIT_DIFF | grep ".*/sdks/c++/.*" | wc -l)
+CPP_SDK_CHANGED=$(echo $GIT_DIFF | grep ".*/basys.sdk.cc/.*" | wc -l)
 
-#if [ $((CPP_SDK_CHANGED > 0)) ];
-#then
+if  ((CPP_SDK_CHANGED > 0));
+then
     echo "Building and testing BaSyx C++ SDK"
 
     mkdir build && cd build
-    cmake ../sdks/c++/basys.sdk.cc -DBASYX_UTILITY_PROJECTS=OFF
+    cmake ../sdks/c++/basys.sdk.cc -DBASYX_UTILITY_PROJECTS=OFF -DBUILD_SHARED_LIBS=ON
     make -j4 all
     ctest
-#fi
+fi
